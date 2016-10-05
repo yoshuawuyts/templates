a0d282a59c6845094e5e4fd680bd298bf44b6ca3
Yoshua Wuyts <yoshuawuyts@gmail.com>
2016-10-05 17:13:09 +0200

    choo: add templates

--- choo/index.js
+++ choo/index.js
@@ -1,15 +1,44 @@
+const persist = require('choo-persist')
+const bulk = require('bulk-require')
+const mount = require('choo/mount')
 const html = require('choo/html')
+const log = require('choo-log')
 const sf = require('sheetify')
 const choo = require('choo')
 
+// css
 sf('tachyons')
 
+// init
+const opts = {}
 const app = choo()
 app.router(['/', mainView])
 
-const tree = app.start()
-document.body.appendChild(tree)
+// logic
+const logic = bulk(__dirname, [ 'models/*', 'actions/*' ])
+Object.keys(logic).forEach((key) => {
+  Object.keys(logic[key]).forEach((key) => {
+    app.model(logic.actions[key](opts))
+  })
+})
+
+// start
+if (process.env.NODE_ENV === 'development') {
+  app.use(log())
+  mount('body', app.start())
+} else {
+  persist((persist) => {
+    app.use(persist)
+    mount('body', app.start())
+  })
+}
 
 function mainView (state, prev, send) {
-  return html`<main>hello world</main>`
+  return html`
+    <body>
+      <main>hello world</main>
+      <link rel="stylesheet" href="bundle.css">
+      <script src="bundle.js"></script>
+    </body>
+  `
 }
