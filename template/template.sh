ecf88430b3ad7954dc3c8fa52db5a67e89339fb7
Yoshua Wuyts <yoshuawuyts@gmail.com>
2016-10-09 17:49:59 +0200

    feat(docker): add

--- template/template.sh
+++ template/template.sh
@@ -1,3 +1,21 @@
+dirname=$(dirname "$(readlink -f "$0")")
+
+readf () {
+  file="$(readlink -f "$dirname/$1")"
+  cat "$file"
+}
+
+replace () {
+  sed "s/{{$1}}/$2/g"
+}
+
+copy () {
+  infile="$1"
+  outfile="$2"
+  tmpfile="$(readlink -f "$dirname/$infile")"
+  cat "$tmpfile" > "$outfile"
+}
+
 if [ $# = "0" ]; then
   printf 'usage: ew NAME\n'
   exit 1
