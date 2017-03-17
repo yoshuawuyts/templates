cc9c7745a12423e73dd368832292d5eaab17ba8c
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-03-18 00:50:22 +0100

    choo v5

--- choo/index.js
+++ choo/index.js
@@ -1,44 +1,27 @@
-const persist = require('choo-persist')
-const bulk = require('bulk-require')
-const mount = require('choo/mount')
-const html = require('choo/html')
-const log = require('choo-log')
-const sf = require('sheetify')
-const choo = require('choo')
+var expose = require('choo-expose')
+var html = require('choo/html')
+var log = require('choo-log')
+var css = require('sheetify')
+var choo = require('choo')
 
-// css
-sf('tachyons')
+css('tachyons')
 
-// init
-const opts = {}
-const app = choo()
-app.router(['/', mainView])
-
-// logic
-const logic = bulk(__dirname, [ 'models/*', 'actions/*' ])
-Object.keys(logic).forEach((key) => {
-  Object.keys(logic[key]).forEach((key) => {
-    app.model(logic.actions[key](opts))
-  })
-})
-
-// start
-if (process.env.NODE_ENV === 'development') {
-  app.use(log())
-  mount('body', app.start())
-} else {
-  persist((persist) => {
-    app.use(persist)
-    mount('body', app.start())
-  })
-}
+var app = choo()
+app.use(log())
+app.use(expose())
+app.route('/', mainView)
+app.mount('body')
 
 function mainView (state, prev, send) {
   return html`
     <body>
+      <header></header>
       <main>
-        <h1 class="f-6">choo choo</h1>
+        <h1 class="f-6">
+          choo choo
+        </h1>
       </main>
+      <footer></footer>
     </body>
   `
 }
