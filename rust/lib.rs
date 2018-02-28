d62c6d975315d8d56aa1004985b27a1927cca56d
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-02-28 15:49:25 +0100

    upgrade rust

---
+++ rust/lib.rs
@@ -0,0 +1,3 @@
+#![deny(warnings, missing_docs)]
+#![cfg_attr(test, feature(plugin))]
+#![cfg_attr(test, plugin(clippy))]
--- rust/main.rs
+++ rust/main.rs
@@ -1,1 +1,5 @@
+#![deny(warnings, missing_docs)]
+#![cfg_attr(test, feature(plugin))]
+#![cfg_attr(test, plugin(clippy))]
 
+fn main() {}
