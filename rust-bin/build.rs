e65613fe21b2e6c4a8bcfa11c10ef906cbb07ea5
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-07-21 16:36:06 +0200

    fix cargo-bin

--- rust-bin/build.rs
+++ rust-bin/build.rs
@@ -6,7 +6,7 @@ include!("src/lib.rs");
 fn main() -> Result<(), Box<std::error::Error>> {
   let outdir = ::std::env::var_os("OUT_DIR").expect("OUT_DIR not found.");
   let mut app = cli::Opts::clap();
-  for shell in Shell::variants().into_iter() {
+  for shell in &Shell::variants() {
     let shell = Shell::from_str(*shell)?;
     app.gen_completions(env!("CARGO_PKG_NAME"), shell, &outdir);
   }
