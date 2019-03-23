e18d1e89ca4e405e5112d7bf4cc938707a52d403
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-03-23 22:35:53 +0100

    desc in lib.rs
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- rust/lib.rs
+++ rust/lib.rs
@@ -1,9 +1,11 @@
-#![forbid(unsafe_code, future_incompatible, rust_2018_idioms)]
-#![deny(missing_debug_implementations, nonstandard_style)]
-#![warn(missing_docs)]
-#![cfg_attr(test, deny(warnings))]
-
+//! {{DESCRIPTION}}
+//!
 //! ## Example
 //!
 //! ```rust
 //! ```
+
+#![forbid(unsafe_code, future_incompatible, rust_2018_idioms)]
+#![deny(missing_debug_implementations, nonstandard_style)]
+#![warn(missing_docs)]
+#![cfg_attr(test, deny(warnings))]
