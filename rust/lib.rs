157c687be0636ccd1ea284af2479b9bfc73aac01
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-04-02 17:44:01 +0200

    missing code doc
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- rust/lib.rs
+++ rust/lib.rs
@@ -7,5 +7,5 @@
 
 #![forbid(unsafe_code, future_incompatible, rust_2018_idioms)]
 #![deny(missing_debug_implementations, nonstandard_style)]
-#![warn(missing_docs)]
+#![warn(missing_docs, missing_doc_code_examples)]
 #![cfg_attr(test, deny(warnings))]
