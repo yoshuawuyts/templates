f9e8f009d8fdc0a25ed752d07de7bf84b6929ecc
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-05-26 12:58:30 +0200

    better pragmas
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- rust-bin/main.rs
+++ rust-bin/main.rs
@@ -1,6 +1,6 @@
-#![forbid(unsafe_code, bad_style, future_incompatible)]
-#![forbid(missing_debug_implementations)]
-#![forbid(missing_docs)]
+#![forbid(unsafe_code, future_incompatible, rust_2018_idioms)]
+#![deny(missing_debug_implementations, nonstandard_style)]
+#![warn(missing_docs, missing_doc_code_examples, unreachable_pub)]
 #![cfg_attr(test, deny(warnings))]
 
 use {{PROJECTNAME}}::Cli;
